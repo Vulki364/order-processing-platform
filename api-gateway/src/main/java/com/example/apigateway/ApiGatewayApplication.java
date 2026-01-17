90ed9608dc9b41c8ae8b9deab6b748120958c0f2
olegzdanov34-vulki <olegzdanov34@gmail.com>
2026-01-17 22:50:07 +0500

    feat(api-gateway): add routing for auth and product services

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -8,6 +8,11 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+        System.out.println("H");
+        System.out.println("E");
+        System.out.println("L");
+        System.out.println("L");
+        System.out.println("O");
     }
 
 }
